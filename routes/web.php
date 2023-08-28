3dfce51ab69d775b5e06ef51a1feb0f804638bdb
ReviDaerobi <nekopoivi1@gmail.com>
2023-08-28 16:56:38 +0700

    ccc
    ccc

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $users = DB::table('users')->get();
+    return view('welcome',['users' => $users]);
 });
